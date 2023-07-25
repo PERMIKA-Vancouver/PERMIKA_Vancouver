4d7d9e3ae49486a38b10fb45af9a7da2b6703558
Julian <julian.widjaja02@gmail.com>
2023-07-25 13:25:39 -0700

    PVW-6: Initial navbar template

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,11 @@
-import WhiteLogo from "../../assets/logo_white.png";
+import Logo from '../../assets/logo.png';
 
 export const Home = () => {
   return (
     <div>
       <div className="App">
         <header className="App-header">
-          <img src={WhiteLogo} className="App-logo" alt="logo" />
+          <img src={Logo} className="App-logo" alt="logo" />
           <p>PERMIKA Vancouver</p>
         </header>
       </div>
--- client/src/shared/components/NavigationBar.tsx
+++ client/src/shared/components/NavigationBar.tsx
@@ -1,10 +1,49 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as NavLogo } from '../../assets/logo.svg';
 
 export const NavigationBar = () => {
   return (
-    <div>
-      <p>Navigation Bar</p>
-      <Outlet />
-    </div>
+    <>
+      <div className="flex justify-between items-center z-10 w-full mt-10 absolute">
+        <div className="ml-6 sm:ml-20 md:ml-6 lg:ml-20">
+          <Link to="/">
+            {/** <NavLogo className="w-14 sm:w-16 h-auto" /> */}
+            <span className="bg-[#D9D9D9] w-14 h-14 block rounded-full" />
+          </Link>
+        </div>
+        <div className="flex mr-18 lg:mr-20">
+          <Link to="/" className="px-6">
+            <p className="text-[#BCBCBC] font-normal hover:text-[#4A4A4A]">
+              about
+            </p>
+          </Link>
+          <Link to="/" className="px-6">
+            <p className="text-[#BCBCBC] font-normal">events</p>
+          </Link>
+          <Link to="/" className="px-6">
+            <p className="text-[#BCBCBC] font-normal">blog</p>
+          </Link>
+          <Link to="/" className="px-6">
+            <p className="text-[#BCBCBC] font-normal">sponsorship</p>
+          </Link>
+        </div>
+      </div>
+      <>
+        <Outlet />
+      </>
+    </>
   );
 };
+
+{
+  /**{pages.map((page) => (
+              <Link key={page.name} to={page.path} className="px-5 pt-3">
+                <p
+                  className={`hover:underline underline-offset-8 decoration-2 font-oswald text-xl text-white`}
+                >
+                  {page.name}
+                </p>
+              </Link>
+            ))} */
+}
