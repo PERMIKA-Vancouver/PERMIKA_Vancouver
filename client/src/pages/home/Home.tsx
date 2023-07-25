03007477969c97411571701c3fdac8a3c04bf3d4
Julian <julian.widjaja02@gmail.com>
2023-07-25 01:42:08 -0700

    PVW-6: Initial navbar setup - routing

---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import WhiteLogo from "../../assets/logo_white.png";
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={WhiteLogo} className="App-logo" alt="logo" />
+          <p>PERMIKA Vancouver</p>
+        </header>
+      </div>
+    </div>
+  );
+};
---
+++ client/src/shared/data/pages.tsx
@@ -0,0 +1,9 @@
+import { Home } from "../../pages/home/Home";
+import { About } from "../../pages/about/About";
+
+const PAGES = [
+  { name: "Home", path: "", element: <Home /> },
+  { name: "About", path: "about", element: <About /> },
+];
+
+export { PAGES };
