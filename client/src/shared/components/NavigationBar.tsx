e6c138f627bf1abb233c742e0b2c43fd27d366b1
Julian <julian.widjaja02@gmail.com>
2023-08-01 00:50:09 -0700

    PVW-6: Fixing margins and adding hover effect on navbar

--- client/src/shared/components/NavigationBar.tsx
+++ client/src/shared/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 import NavLogo from '../../assets/cropped_logo.png';
 
@@ -7,19 +7,25 @@ import { PAGES } from '../data/pages';
 export const NavigationBar = () => {
   return (
     <>
-      <div className="flex justify-between items-center z-10 w-full mt-10 absolute">
+      <div className="flex justify-between items-center z-10 w-full mt-[3.125rem] absolute">
         <div className="ml-16">
           <Link to="/">
             <img src={NavLogo} alt="" className="w-[55px] h-auto" />
           </Link>
         </div>
-        <div className="flex mr-10">
+        <div className="flex mr-16 gap-8">
           {PAGES.filter((page) => page.name !== 'home').map((page) => (
-            <Link key={page.name} to={page.path} className="px-6">
-              <p className="text-[#BCBCBC] font-normal hover:text-[#4A4A4A]">
-                {page.name}
-              </p>
-            </Link>
+            <NavLink key={page.name} to={page.path}>
+              {({ isActive, isPending }) => (
+                <p
+                  className={`${
+                    isActive ? 'text-[#0A0A0A]' : 'text-[#BCBCBC]'
+                  } font-normal hover:text-[#D07D14]`}
+                >
+                  {page.name}
+                </p>
+              )}
+            </NavLink>
           ))}
         </div>
       </div>
