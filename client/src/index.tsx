9c17b9ce4b312eacf14936075eb3b4280a127ed1
Julian <julian.widjaja02@gmail.com>
2023-08-02 02:12:55 -0700

    PVW-6: Add Avenir & Regola font

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// fonts
+import './fonts/Regola/regola-regular.ttf';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
