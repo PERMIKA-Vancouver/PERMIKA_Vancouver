7b054c9c8f598f557d84237947fd58aeae1b8dcb
Julian <julian.widjaja02@gmail.com>
2023-08-24 00:52:03 -0700

    PVW-10: Create border line in footer

--- client/src/shared/components/Footer.tsx
+++ client/src/shared/components/Footer.tsx
@@ -13,12 +13,13 @@ export const Footer = () => {
   return (
     <div className={`relative w-full bg-[#F8F8F8] h-[15vh]`}>
       <div
-        className={`absolute top-[50%] -translate-y-2/4 flex justify-between items-center w-full`}
+        className={`absolute top-[50%] -translate-y-2/4 flex items-center w-full h-full`}
       >
         <div className="ml-[4%]">
           <span className="t1 text-[#5B5B5B]">PERMIKA Vancouver 2023</span>
         </div>
-        <div className="flex gap-[1.875rem] mr-[4%]">
+        <div className="border-l-2 border-solid border-[#969696] h-[65%] self-center mr-[9%] ml-auto"></div>
+        <div className="flex gap-[1.875rem] h-[24%] self-center mr-[4%]">
           <FaInstagram className={logoStyling} />
           <FaDiscord className={logoStyling} />
           <FaYoutube className={logoStyling} />
