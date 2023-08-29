07b084774fc9a58eeb36d47db8d8cd70c1c72566
Julian <julian.widjaja02@gmail.com>
2023-08-28 23:01:41 -0700

    PVW-9: Change the cursor to pointer for custom button

--- client/src/shared/components/CustomButton.tsx
+++ client/src/shared/components/CustomButton.tsx
@@ -13,7 +13,7 @@ export const CustomButton = ({
 
   return (
     <div
-      className={`bg-sunset-orange w-[12.875rem] h-[2.1875rem] rounded-[0.1875rem] hover:bg-[#A86108] transition-colors ease-out duration-300 ${className} flex items-center justify-center`}
+      className={`bg-sunset-orange w-[12.875rem] h-[2.1875rem] rounded-[0.1875rem] hover:bg-[#A86108] transition-colors ease-out duration-300 ${className} flex items-center justify-center cursor-pointer`}
       onClick={clickHandler}
     >
       <span className="text-white button-text">{text}</span>
