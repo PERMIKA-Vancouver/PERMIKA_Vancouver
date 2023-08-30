04e9d34b8fa2b8c2caedb1672b46ab4e811b6a13
Julian <julian.widjaja02@gmail.com>
2023-08-29 21:06:33 -0700

    PVW-8: Added the custom button to welcoming message

--- client/src/pages/home/components/WelcomingMessage.tsx
+++ client/src/pages/home/components/WelcomingMessage.tsx
@@ -1,4 +1,5 @@
 import Core from '../../../assets/core.png';
+import { CustomButton } from '../../../shared/components/CustomButton';
 
 const WelcomingMsg =
   "Hello there! Welcome to PERMIKA Vancouver's new chapter. We're so excited for you to experience both our signature and new events in this coming year. We also hope to create a community that you can call home while living in Vancouver. We're so glad you're here!";
@@ -16,7 +17,11 @@ export const WelcomingMessage = () => {
           <br />
           Tell us a bit about yourself and become a part of Permika Vancouver!
         </p>
-        <span>Custom Button</span>
+        <CustomButton
+          text="Join Permika"
+          className=""
+          link="https://www.linkedin.com/in/julian-widjaja/"
+        />
       </div>
       <div>
         <img src={Core} alt="Core Team" className="max-h-[60vh] w-auto" />
