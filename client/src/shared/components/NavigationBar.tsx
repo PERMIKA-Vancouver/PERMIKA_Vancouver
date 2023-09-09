f1ac5fcb790e2f79ebad90fd69a89078fc6f9d0b
Julian <julian.widjaja02@gmail.com>
2023-09-08 22:18:31 -0700

    PVW-13: Added a mobile view breakpoint for navbar

--- client/src/shared/components/NavigationBar.tsx
+++ client/src/shared/components/NavigationBar.tsx
@@ -5,10 +5,13 @@ import NavLogo from '../../assets/cropped_logo.png';
 
 import { PAGES } from '../data/pages';
 import { Footer } from './Footer';
+import { SandwichNavbar } from './SandwichNavbar';
+import { MOBILE_WIDTH } from '../data/common';
 
 export const NavigationBar = () => {
   const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const controlNavbar = useCallback(() => {
     if (typeof window !== 'undefined') {
@@ -36,6 +39,10 @@ export const NavigationBar = () => {
     }
   }, [lastScrollY, controlNavbar]);
 
+  useEffect(() => {
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+  }, []);
+
   return (
     <>
       <div
@@ -44,26 +51,30 @@ export const NavigationBar = () => {
         } h-[20vh] w-full bg-white transition-[top] ease-in duration-500`}
       >
         <div className="absolute top-[50%] -translate-y-2/4 flex justify-between items-center w-full">
-          <div className="ml-[4%]">
+          <div className="ml-[10%] sm:ml-[4%]">
             <Link to="/">
               <img src={NavLogo} alt="" className="w-[55px] h-auto" />
             </Link>
           </div>
-          <div className="flex mr-[4%] gap-8">
-            {PAGES.filter((page) => page.name !== 'home').map((page) => (
-              <NavLink key={page.name} to={page.path}>
-                {({ isActive }) => (
-                  <p
-                    className={`RegoBook ${
-                      isActive ? 'text-black-permika' : 'text-light-grey'
-                    } hover:text-sunset-orange font-normal not-italic text-lg leading-[normal] transition duration-500`}
-                  >
-                    {page.name}
-                  </p>
-                )}
-              </NavLink>
-            ))}
-          </div>
+          {windowWidth < MOBILE_WIDTH ? (
+            <SandwichNavbar />
+          ) : (
+            <div className="flex mr-[4%] gap-8">
+              {PAGES.filter((page) => page.name !== 'home').map((page) => (
+                <NavLink key={page.name} to={page.path}>
+                  {({ isActive }) => (
+                    <p
+                      className={`RegoBook ${
+                        isActive ? 'text-black-permika' : 'text-light-grey'
+                      } hover:text-sunset-orange font-normal not-italic text-lg leading-[normal] transition duration-500`}
+                    >
+                      {page.name}
+                    </p>
+                  )}
+                </NavLink>
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <>
