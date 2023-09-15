f22a6974a257df62cd6b01fee8879504e0238491
Filipo Marcellino <fmarcellino7@gmail.com>
2023-09-14 19:11:47 -0700

    add 2 images fade out

--- client/src/shared/components/LandingImage.tsx
+++ client/src/shared/components/LandingImage.tsx
@@ -1,10 +1,12 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import img2 from "../../assets/DSC01469.JPG";
-import img3 from "../../assets/DSC01479.JPG";
-import img4 from "../../assets/DSC01487.JPG";
-import img5 from "../../assets/DSC01610.JPG";
-import img6 from "../../assets/DSC01614.JPG";
-import img7 from "../../assets/DSC01481.JPG";
+import img1 from "../../assets/tuxpi.com.1694742973-min.jpg"
+import img2 from "../../assets/DSC01469-min.JPG";
+import img3 from "../../assets/DSC01479-min.JPG";
+import img4 from "../../assets/DSC01487-min.JPG";
+import img5 from "../../assets/DSC01610-min.JPG";
+import img6 from "../../assets/DSC01614-min.JPG";
+import img7 from "../../assets/DSC01481-min.JPG";
+import img8 from "../../assets/tuxpi.com.1694743036-min.jpg";
 import React from "react";
 
 const LandingImage: React.FC = () => {
@@ -14,6 +16,8 @@ const LandingImage: React.FC = () => {
   const img4Ref = useRef<HTMLImageElement>(null);
   const img5Ref = useRef<HTMLImageElement>(null);
   const img6Ref = useRef<HTMLImageElement>(null);
+  const img7Ref = useRef<HTMLImageElement>(null);
+  const img8Ref = useRef<HTMLImageElement>(null);
 
   // Function to update image positions
   const updateImagesPosition = useCallback((cursorPosition: { x: number, y: number }) => {
@@ -31,6 +35,9 @@ const LandingImage: React.FC = () => {
     applyStyleToImage(img4Ref.current, 35, 83);
     applyStyleToImage(img5Ref.current, 28, 29);
     applyStyleToImage(img6Ref.current, 76, 75);
+    applyStyleToImage(img7Ref.current, 95, 55);
+    applyStyleToImage(img8Ref.current, 95, 30);
+
   }, []);
 
   // Mouse Move Handler
@@ -52,8 +59,6 @@ const LandingImage: React.FC = () => {
   }, [mouseMoveHandler]);
 
   const calculatePosition = (initialTop: number, initialLeft: number, cursorPosition: { x: number; y: number }) => {
-    const navbarSize = 200 / window.innerHeight;
-
     let relativeCursorX = cursorPosition.x / window.innerWidth;
     let relativeCursorY = cursorPosition.y / window.innerHeight;
 
@@ -65,14 +70,8 @@ const LandingImage: React.FC = () => {
 
     let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
 
-    let top = initialTop - distanceY * distance * 5;
-    let left = initialLeft - distanceX * distance * 5;
-
-    const imageSizeRelative = 1 / 5;
-    top = Math.min(
-        Math.max(top, navbarSize * 100),
-        100 - imageSizeRelative * 100
-    );
+    let top = initialTop - distanceY * distance * 3;
+    let left = initialLeft - distanceX * distance * 3;
 
     return { top: `${top}%`, left: `${left}%` };
   };
@@ -86,41 +85,53 @@ const LandingImage: React.FC = () => {
   }, []);
 
   return (
-      <div className="relative h-screen">
+      <div className="relative h-screen mb-15">
         <div className="absolute inset-0 z-10">
           <img
               ref={img1Ref}
               src={img7}
               alt="background"
-              className="absolute w-1/6 opacity-50 transition-all duration-300 ease-out"
+              className="absolute w-1/6 opacity-60 transition-all duration-300 ease-out"
           />
           <img
               ref={img2Ref}
               src={img2}
               alt="background"
-              className="absolute w-1/6 opacity-50 transition-all duration-300 ease-out"
+              className="absolute w-1/6 opacity-40 transition-all duration-300 ease-out"
           />
           <img
               ref={img3Ref}
               src={img3}
               alt="background"
-              className="absolute h-2/6 opacity-50 transition-all duration-300 ease-out"
+              className="absolute h-2/6 opacity-60 transition-all duration-300 ease-out"
           />
           <img
               ref={img4Ref}
               src={img4}
               alt="background"
-              className="absolute w-1/6 opacity-50 transition-all duration-300 ease-out"
+              className="absolute w-1/6 opacity-60 transition-all duration-300 ease-out"
           />
           <img
               ref={img5Ref}
               src={img5}
               alt="background"
-              className="absolute w-1/6 opacity-50 transition-all duration-300 ease-out"
+              className="absolute w-1/6 opacity-60 transition-all duration-300 ease-out"
           />
           <img
               ref={img6Ref}
               src={img6}
+              alt="background"
+              className="absolute w-1/6 opacity-60 transition-all duration-300 ease-out"
+          />
+          <img
+              ref={img7Ref}
+              src={img1}
+              alt="background"
+              className="absolute w-1/6 opacity-50 transition-all duration-300 ease-out"
+          />
+          <img
+              ref={img8Ref}
+              src={img8}
               alt="background"
               className="absolute w-1/6 opacity-50 transition-all duration-300 ease-out"
           />
---
+++ client/src/shared/components/TestComponent.tsx
@@ -0,0 +1,9 @@
+const TestComponent: React.FC = () => {
+    return (
+        <div className="h-screen p-28">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloremque esse eveniet iure nemo perspiciatis, repellat veritatis? Deleniti ea ipsa iusto nostrum reiciendis unde? Aut cupiditate error expedita quod quos?
+        </div>
+    );
+};
+
+export default TestComponent;
