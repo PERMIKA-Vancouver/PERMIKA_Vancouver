cca885f21d347f5b7afa645a6e02d6352af21d53
Julian <julian.widjaja02@gmail.com>
2023-09-18 23:01:17 -0700

    PVW-13: Add feature where menu closed when clicked outside

--- client/src/shared/components/navigation-bar/SandwichNavbar.tsx
+++ client/src/shared/components/navigation-bar/SandwichNavbar.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import SandwichLogo from './SandwichLogo';
 import { MenuInterface } from './MenuInterface';
 
 export const SandwichNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
   const handleClose = () => {
     setIsOpen(false);
   };
@@ -12,8 +14,16 @@ export const SandwichNavbar = () => {
     setIsOpen(true);
   };
 
+  const handleClickOutside = (e: any) => {
+    if (isOpen && !menuRef.current?.contains(e.target as Node)) {
+      handleClose();
+    }
+  };
+
+  window.addEventListener('click', handleClickOutside);
+
   return (
-    <div className="mr-[10%] relative">
+    <div className="mr-[10%] relative" ref={menuRef}>
       <SandwichLogo
         isOpen={isOpen}
         openHandler={handleOpen}
