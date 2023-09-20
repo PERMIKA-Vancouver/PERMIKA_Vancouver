574b969fb5ab041d765ebf5b276b875d2babebb7
Filipo Marcellino <fmarcellino7@gmail.com>
2023-09-20 15:21:39 -0700

    move LandingImage component to pages/home/components

--- client/src/pages/home/components/LandingImage.tsx
+++ client/src/pages/home/components/LandingImage.tsx
@@ -1,12 +1,12 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import img1 from "../../assets/tuxpi.com.1694742973-min.webp"
-import img2 from "../../assets/DSC01469-min.webp";
-import img3 from "../../assets/DSC01619-min.webp";
-import img4 from "../../assets/DSC01487-min.webp";
-import img5 from "../../assets/DSC01610-min.webp";
-import img6 from "../../assets/DSC01614-min.webp";
-import img7 from "../../assets/DSC01481-min.webp";
-import img8 from "../../assets/tuxpi.com.1694743036-min.webp";
+import img1 from "../../../assets/tuxpi.com.1694742973-min.webp"
+import img2 from "../../../assets/DSC01469-min.webp";
+import img3 from "../../../assets/DSC01619-min.webp";
+import img4 from "../../../assets/DSC01487-min.webp";
+import img5 from "../../../assets/DSC01610-min.webp";
+import img6 from "../../../assets/DSC01614-min.webp";
+import img7 from "../../../assets/DSC01481-min.webp";
+import img8 from "../../../assets/tuxpi.com.1694743036-min.webp";
 
 const LandingImage: React.FC = () => {
   const img1Ref = useRef<HTMLImageElement>(null);
