bc200e8a4b3aafcc96a935730dd5386b96f23653
Filipo Marcellino <fmarcellino7@gmail.com>
2023-09-20 15:19:20 -0700

    remove testComponent

--- client/src/shared/components/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-const TestComponent: React.FC = () => {
-    return (
-        <div className="h-screen p-28">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloremque esse eveniet iure nemo perspiciatis, repellat veritatis? Deleniti ea ipsa iusto nostrum reiciendis unde? Aut cupiditate error expedita quod quos?
-        </div>
-    );
-};
-
-export default TestComponent;
