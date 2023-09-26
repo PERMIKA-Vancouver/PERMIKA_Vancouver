fae0a880efe125510a9d60bb410455ac2f061de5
Filipo Marcellino <fmarcellino7@gmail.com>
2023-09-25 23:14:38 -0700

    adjust home page after rebase

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,9 +2,13 @@ import { WelcomingMessage } from './components/WelcomingMessage';
 import { CountdownEvent } from './components/CountdownEvent';
 import { useEffect, useState } from 'react';
 import { MOBILE_WIDTH, TABLET_POTRAIT_WIDTH } from '../../shared/data/common';
+import LandingImage from "./components/LandingImage";
+import LandingImageSmall from "./components/LandingImageSmall";
+import {useMediaQuery} from "react-responsive"; // replace with your component
 
 export const Home = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const isSmallScreen = useMediaQuery({ maxWidth: 1023 }); // Adjust the values as per your tailwindCSS configuration
 
   useEffect(() => {
     window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
@@ -12,15 +16,8 @@ export const Home = () => {
 
   return (
     <>
-      <div className={`pt-navbar h-screen bg-light-grey`}>
-        <header className={`text-center text-2xl`}>
-          <p>PERMIKA Vancouver</p>
-        </header>
-      </div>
-      <WelcomingMessage
-        isMobileView={windowWidth < MOBILE_WIDTH}
-        isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
-      />
+        {isSmallScreen ? <LandingImageSmall /> : <LandingImage />}
+      <WelcomingMessage isMobileView={windowWidth < MOBILE_WIDTH} />
       <CountdownEvent
         isMobileView={windowWidth < MOBILE_WIDTH}
         isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
