dc6c823808cc75d5d04590efd48249ec561e059f
Julian <julian.widjaja02@gmail.com>
2023-09-30 18:30:35 -0700

    PVW-18: Finished implementing the upcoming layout

--- client/src/shared/layouts/UpcomingPage.tsx
+++ client/src/shared/layouts/UpcomingPage.tsx
@@ -6,37 +6,55 @@ import {
   openTiktok,
 } from '../utils/OpenLinkUtil';
 
+import Cloud from '../../assets/cloud.svg';
+
 const socialStyle = 'text-sunset-orange cursor-pointer';
 
 export const UpcomingPage = () => {
   return (
     <div className="pt-navbar h-[85vh]">
-      <h1>
-        Hello, you're early! This page is currently undergoing renovations..
-      </h1>
-      <h4 className={`text-[#9a9a9a]`}>
-        It will be up soon but in the meantime, you can find us on{' '}
-        <span className={socialStyle} onClick={openInstagram}>
-          instagram
-        </span>
-        ,{' '}
-        <span className={socialStyle} onClick={openDiscord}>
-          discord
-        </span>
-        ,{' '}
-        <span className={socialStyle} onClick={openYoutube}>
-          youtube
-        </span>
-        ,{' '}
-        <span className={socialStyle} onClick={openFacebook}>
-          facebook
-        </span>
-        , or{' '}
-        <span className={socialStyle} onClick={openTiktok}>
-          TikTok
-        </span>
-        !
-      </h4>
+      <div className="ml-all mr-[10%] h-full">
+        <div className="relative h-[40%]">
+          <img
+            src={Cloud}
+            alt="cloud"
+            className={`absolute bottom-0 left-0 w-[15%]`}
+          />
+          <img
+            src={Cloud}
+            alt="cloud"
+            className={`absolute top-0 left-52 w-[15%]`}
+          />
+        </div>
+        <div className="mt-[5%]">
+          <h2>
+            Hello, you're early! This page is currently undergoing renovations..
+          </h2>
+          <p className={`text-[#9a9a9a] mt-5`}>
+            It will be up soon but in the meantime, you can find us on{' '}
+            <span className={socialStyle} onClick={openInstagram}>
+              instagram
+            </span>
+            ,{' '}
+            <span className={socialStyle} onClick={openDiscord}>
+              discord
+            </span>
+            ,{' '}
+            <span className={socialStyle} onClick={openYoutube}>
+              youtube
+            </span>
+            ,{' '}
+            <span className={socialStyle} onClick={openFacebook}>
+              facebook
+            </span>
+            , or{' '}
+            <span className={socialStyle} onClick={openTiktok}>
+              TikTok
+            </span>
+            !
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
