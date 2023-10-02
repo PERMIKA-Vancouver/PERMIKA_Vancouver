6a0df40d0552c518bb38ee731bb5d40317d6cc8f
Julian <julian.widjaja02@gmail.com>
2023-10-01 18:38:20 -0700

    PVW-17: Update home page

--- client/src/shared/data/pages.tsx
+++ client/src/shared/data/pages.tsx
@@ -1,7 +1,8 @@
+import { Home } from '../../pages/home/Home';
 import { UpcomingPage } from '../layouts/UpcomingPage';
 
 const PAGES = [
-  { name: 'home', path: '', element: <UpcomingPage /> },
+  { name: 'home', path: '', element: <Home /> },
   { name: 'about', path: 'about', element: <UpcomingPage /> },
   { name: 'events', path: 'events', element: <UpcomingPage /> },
   { name: 'blog', path: 'blog', element: <UpcomingPage /> },
