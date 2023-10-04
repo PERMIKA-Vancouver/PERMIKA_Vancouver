890bc7bff1989ae40392a550ce2f1fd38224d53d
Julian <julian.widjaja02@gmail.com>
2023-10-03 23:05:18 -0700

    PVW-25: Initial setup of seo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,12 +5,15 @@ import App from './App';
 
 // fonts
 import './fonts/Regola/regola-regular.ttf';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
