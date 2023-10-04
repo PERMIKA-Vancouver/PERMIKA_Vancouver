9a1b53dbe0f70542062cbe046d7ea4154ae3d021
Julian <julian.widjaja02@gmail.com>
2023-10-03 23:09:36 -0700

    PVW-25: Add helmet on home

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,13 +2,14 @@ import { WelcomingMessage } from './components/WelcomingMessage';
 import { CountdownEvent } from './components/CountdownEvent';
 import { useEffect, useState } from 'react';
 import { MOBILE_WIDTH, TABLET_POTRAIT_WIDTH } from '../../shared/data/common';
-import LandingImage from "./components/LandingImage";
-import LandingImageSmall from "./components/LandingImageSmall";
-import {useMediaQuery} from "react-responsive"; // replace with your component
+import LandingImage from './components/LandingImage';
+import LandingImageSmall from './components/LandingImageSmall';
+import { useMediaQuery } from 'react-responsive'; // replace with your component
+import { Helmet } from 'react-helmet-async';
 
 export const Home = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const isSmallScreen = useMediaQuery({ maxWidth: 1023 }); // Adjust the values as per your tailwindCSS configuration
+  const isSmallScreen = useMediaQuery({ maxWidth: 1023 }); // Adjust the values as per your tailwindCSS configuration
 
   useEffect(() => {
     window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
@@ -16,15 +17,26 @@ export const Home = () => {
 
   return (
     <>
-        {isSmallScreen ? <LandingImageSmall /> : <LandingImage />}
-        <WelcomingMessage
-            isMobileView={windowWidth < MOBILE_WIDTH}
-            isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
+      <Helmet>
+        <title>PERMIKA Vancouver | Home</title>
+        <meta
+          name="description"
+          content="A student-led organization that unites all Indonesians in
+    post-secondary institutions across the Greater Vancouver area. We
+    strive to bring a part of Indonesia through our events and services to
+    your new life here!"
         />
-        <CountdownEvent
-            isMobileView={windowWidth < MOBILE_WIDTH}
-            isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
-        />
+        <link rel="canocical" href="/" />
+      </Helmet>
+      {isSmallScreen ? <LandingImageSmall /> : <LandingImage />}
+      <WelcomingMessage
+        isMobileView={windowWidth < MOBILE_WIDTH}
+        isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
+      />
+      <CountdownEvent
+        isMobileView={windowWidth < MOBILE_WIDTH}
+        isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
+      />
     </>
   );
 };
