3a1bb4a8bc8f2aa392db3a12f0f0519a5759183e
Julian <julian.widjaja02@gmail.com>
2023-11-20 02:23:42 -0800

    PVW-30: Temporary design evaluation

--- client/src/pages/home/components/CountdownEvent.tsx
+++ client/src/pages/home/components/CountdownEvent.tsx
@@ -91,6 +91,68 @@ export const CountdownEvent = ({
             </>
           </div>
         )}
+        {/** TEMPORARY */}
+        <div className="mt-40"></div>
+        <div
+          className={`${
+            isMobileView ? 'mt-4' : 'flex mt-[2%]'
+          } justify-between`}
+        >
+          <div className="flex items-center">
+            {!isMobileView && (
+              <div
+                className={`mr-4 bg-light-green w-[1.4375rem] h-[0.4375rem] rounded-[0.0625rem]`}
+              ></div>
+            )}
+            <>
+              <h3 className={`${isMobileView ? 'mt-4' : 'sub'} text-[#8CA080]`}>
+                {nextEventName}
+              </h3>
+            </>
+          </div>
+          <>
+            <p
+              className={`${
+                isMobileView
+                  ? 'mt-2'
+                  : 'font-AveRom text-[1.3125rem] italic font-normal leading-[124.6%] tracking-[-0.02625rem]'
+              } text-[#8CA080]`}
+            >
+              {nextEventDate}
+            </p>
+          </>
+        </div>
+        <div
+          className={`${
+            isMobileView
+              ? 'mt-[4.3rem] mb-[5.25rem]'
+              : isTabletPotraitView
+              ? 'my-[5.75rem]'
+              : 'mt-[9%] mb-[7%]'
+          }`}
+        >
+          <CountdownTimer
+            countdownTimestamp={nextEventTimestamp}
+            isMobileView={isMobileView}
+            isTabletPotraitView={isTabletPotraitView}
+          />
+        </div>
+        {isNextEvent === 'true' && (
+          <div className="text-center">
+            <div className="mb-4">
+              <span className={`font-AveRom text-[#E3E3E3] text-[1rem]`}>
+                {nextEventDescription}
+              </span>
+            </div>
+            <>
+              <CustomButton
+                text={nextEventButtonText}
+                className="m-auto"
+                link={nextEventLink}
+              />
+            </>
+          </div>
+        )}
       </div>
     </div>
   );
