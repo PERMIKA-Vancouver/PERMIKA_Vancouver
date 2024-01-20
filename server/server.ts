2eb37a4654095e4648f7a84a46004b57bcc59b59
Julian <julian.widjaja02@gmail.com>
2024-01-20 01:46:14 -0800

    PVW-35: Update server from js to ts

--- server/server.ts
+++ server/server.ts
@@ -3,27 +3,19 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 dotenv.config();
+const MONGODB: string = process.env.MONGODB ?? '';
 
 // express app
 const app = express();
 
-// middleware
-// app.use((req, rest, next) => {
-//   console.log(req.path, req.method);
-//   next();
-// });
-
-// routes
-// app.use(merchandiseRoutes);
-
 app.get('/', (req, res) => {
   console.log(req.method, req.path);
-  return res.status(234).send('Welcone to PERMIKA');
+  return res.status(234).send('Welcome to PERMIKA!!!!!');
 });
 
 // connect to mongodb
 mongoose
-  .connect(process.env.MONGODB)
+  .connect(MONGODB)
   .then(() => {
     console.log('App connected to database');
     app.listen(process.env.PORT, () => {
