f53644c99ed5591db72c8c50e48afe7fc6850be6
Julian <julian.widjaja02@gmail.com>
2024-01-20 18:33:28 -0800

    PVW-35: Setup initial connection

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 dotenv.config();
 const MONGODB: string = process.env.MONGODB ?? '';
 
 // express app
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (req, res) => {
   console.log(req.method, req.path);
