3e17c982a349039b17e1375a2eb8cea8fb26b694
Filipo Marcellino <fmarcellino7@gmail.com>
2024-01-30 13:23:00 -0800

    merchandise section base appearance

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,18 +1,19 @@
-import { WelcomingMessage } from './components/WelcomingMessage';
-import { CountdownEvent } from './components/CountdownEvent';
-import { useEffect, useState } from 'react';
-import { MOBILE_WIDTH, TABLET_POTRAIT_WIDTH } from '../../shared/data/common';
-import LandingImage from './components/LandingImage';
-import LandingImageSmall from './components/LandingImageSmall';
-import { useMediaQuery } from 'react-responsive'; // replace with your component
-import { Helmet } from 'react-helmet-async';
+import { WelcomingMessage } from "./components/WelcomingMessage";
+import { CountdownEvent } from "./components/CountdownEvent";
+import { useEffect, useState } from "react";
+import { MOBILE_WIDTH, TABLET_POTRAIT_WIDTH } from "../../shared/data/common";
+import LandingImage from "./components/LandingImage";
+import LandingImageSmall from "./components/LandingImageSmall";
+import { useMediaQuery } from "react-responsive"; // replace with your component
+import { Helmet } from "react-helmet-async";
+import { Merchandise } from "./components/Merchandise";
 
 export const Home = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const isSmallScreen = useMediaQuery({ maxWidth: 1023 }); // Adjust the values as per your tailwindCSS configuration
 
   useEffect(() => {
-    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
   }, []);
 
   return (
@@ -37,6 +38,7 @@ export const Home = () => {
         isMobileView={windowWidth < MOBILE_WIDTH}
         isTabletPotraitView={windowWidth < TABLET_POTRAIT_WIDTH}
       />
+      <Merchandise />
     </>
   );
 };
---
+++ client/src/pages/home/components/Merchandise.tsx
@@ -0,0 +1,61 @@
+import sample from "../../../assets/merchandise/hoodie.png";
+import { CustomButton } from "../../../shared/components/CustomButton";
+
+export const Merchandise = () => {
+  return (
+    <div className="flex flex-col p-28 gap-15">
+      <div className="flex justify-between">
+        <h3>Merchandise</h3>
+        <CustomButton
+          text="Buy"
+          className=""
+          link="https://forms.gle/z8AFC5m5PiJzKBEf7"
+        />{" "}
+      </div>
+      <div className="flex gap-20">
+        <div className="flex flex-col text-grey-body">
+          <img src={sample} alt="background" className="w-full" />
+          <p className="text-sm mt-3">Hoodie Collection</p>
+          <p className="text-xl text-black-text">The PERMIKA Hoodie</p>
+          <p className="text-sm">$45</p>
+          <div className="flex justify-between mt-4">
+            <p className="text-sm text-black text bg-[#F1F1F1] py-1 px-3.5">
+              5 Sizes - XS, S, M, L, XL
+            </p>
+            <p className="text-sm italic underline text-[#4F4F4F] py-1">
+              Size & Fit
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col text-grey-body">
+          <img src={sample} alt="background" className="w-full" />
+          <p className="text-sm mt-3">Hoodie Collection</p>
+          <p className="text-xl text-black-text">The PERMIKA Hoodie</p>
+          <p className="text-sm">$45</p>
+          <div className="flex justify-between mt-4">
+            <p className="text-sm text-black text bg-[#F1F1F1] py-1 px-3.5">
+              5 Sizes - XS, S, M, L, XL
+            </p>
+            <p className="text-sm italic underline text-[#4F4F4F] py-1">
+              Size & Fit
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col text-grey-body">
+          <img src={sample} alt="background" className="w-full" />
+          <p className="text-sm mt-3">Hoodie Collection</p>
+          <p className="text-xl text-black-text">The PERMIKA Hoodie</p>
+          <p className="text-sm">$45</p>
+          <div className="flex justify-between mt-4">
+            <p className="text-sm text-black text bg-[#F1F1F1] py-1 px-3.5">
+              5 Sizes - XS, S, M, L, XL
+            </p>
+            <p className="text-sm italic underline text-[#4F4F4F] py-1">
+              Size & Fit
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
