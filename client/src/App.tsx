f619dae9bc978074ecc2e46222e0c4c1ea984de3
Julian <julian.widjaja02@gmail.com>
2024-01-29 23:51:48 -0800

    PVW-36: Fix some stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,28 @@
 import './App.css';
 
-import * as reactRouterDom from 'react-router-dom';
-
 import { PAGES } from './shared/data/pages';
 import { NavigationBar } from './shared/components/navigation-bar/NavigationBar';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <reactRouterDom.BrowserRouter>
-        <reactRouterDom.Routes>
-          <reactRouterDom.Route path="/" element={<NavigationBar />}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<NavigationBar />}>
             {PAGES.map((page) => (
-              <reactRouterDom.Route
-                key={page.name}
-                path={page.path}
-                element={page.element}
-              />
+              <Route key={page.name} path={page.path} element={page.element} />
             ))}
-          </reactRouterDom.Route>
-          <reactRouterDom.Route
-            path="*"
-            element={<reactRouterDom.Navigate replace to="/" />}
-          />
-        </reactRouterDom.Routes>
-      </reactRouterDom.BrowserRouter>
+          </Route>
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- client/src/pages/order/components/ShoppingBag.tsx
+++
@@ -1,3 +0,0 @@
-export const ShoppingBag = () => {
-  return <div className = "w-[38%]">Shopping Bag</div>;
-};
