d6f41835c736af743f0d8484505a1f34f3b5a08d
Erick Gunawan <erick.gunawan@runchise.com>
2024-07-07 15:07:47 -0600

    add PDO RSVP

--- client/src/shared/data/events.ts
+++ client/src/shared/data/events.ts
@@ -1,33 +1,33 @@
-import { UpcomingEvent } from '../types/types';
+import { UpcomingEvent } from "../types/types";
 
-const DATE_FORMAT = 'YYYY-MM-DD HH:mm';
+const DATE_FORMAT = "YYYY-MM-DD HH:mm";
 
 // Date format 'YYYY-MM-DD HH:mm'
 const UPCOMING_EVENTS: UpcomingEvent[] = [
   {
-    name: 'PERMIKA Hiring',
-    date: '2024-05-13 23:59',
-    rsvp: 'https://docs.google.com/forms/d/e/1FAIpQLSc8ZMQzcCdszKmZICP2SHmZtN2Dg56Y6Or_sUJJ69jG01U17w/viewform',
-    location: '',
-    locationLink: '',
-    type: 1,
+    name: "Pre-Departure Orientation",
+    date: "2024-07-19 20:00",
+    rsvp: "https://forms.gle/VopZFSiGvv5go9Vc7",
+    location: "",
+    locationLink: "",
+    type: 0,
   },
 ];
 
 const UPCOMING_EVENTS_DEFAULT: UpcomingEvent = {
-  name: 'No Upcoming Events',
-  date: '0000-00-00 00:00',
-  rsvp: '',
-  location: '',
-  locationLink: '',
+  name: "No Upcoming Events",
+  date: "0000-00-00 00:00",
+  rsvp: "",
+  location: "",
+  locationLink: "",
   type: 0,
 };
 
-const EVENT_DESCRIPTION = 'Save yourself a seat for the event below!';
-const EVENT_BUTTON = 'RSVP';
+const EVENT_DESCRIPTION = "Save yourself a seat for the event below!";
+const EVENT_BUTTON = "RSVP";
 
 const HIRING_DESCRIPTION = "Be part of PERMIKA's family 2024/2025";
-const HIRING_BUTTON = 'Join PERMIKA';
+const HIRING_BUTTON = "Join PERMIKA";
 
 export {
   DATE_FORMAT,
