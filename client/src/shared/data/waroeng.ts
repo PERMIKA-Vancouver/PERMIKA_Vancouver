466e7415897d87237053704ca85dfb5d76357afa
Julian <84741001+Julian-UBC@users.noreply.github.com>
2024-07-21 14:16:30 -0700

    Update ketoprak time

--- client/src/shared/data/waroeng.ts
+++ client/src/shared/data/waroeng.ts
@@ -6,7 +6,7 @@ const DATE_FORMAT = 'yyyy-MM-dd HH:mm';
 // Date format 'YYYY-MM-DD HH:mm'
 const UPCOMING_WAROENG: UpcomingWaroeng[] = [
   {
-    date: DateTime.fromFormat('2024-07-26 23:00', DATE_FORMAT, { zone: 'America/Los_Angeles' }),
+    date: DateTime.fromFormat('2024-07-26 23:59', DATE_FORMAT, { zone: 'America/Los_Angeles' }),
     pickup: DateTime.fromFormat('2024-07-28 16:00', DATE_FORMAT, { zone: 'America/Los_Angeles' }),
     link: 'https://forms.gle/yYS7zPBCVmEPcAqR6',
   },
