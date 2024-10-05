9d15d3483f19a76c99c94d1c3ab92a0cd335a768
Reynella Liebig <reynella@tapta.lk>
2024-10-05 16:59:54 -0700

    waroeng24 ayam geprek updates; countdown and pictures.

--- client/src/shared/data/waroeng.ts
+++ client/src/shared/data/waroeng.ts
@@ -6,10 +6,10 @@ const DATE_FORMAT = 'yyyy-MM-dd HH:mm';
 // Date format 'YYYY-MM-DD HH:mm'
 const UPCOMING_WAROENG: UpcomingWaroeng[] = [
   {
-    date: DateTime.fromFormat('2024-10-11 23:59', DATE_FORMAT, {
+    date: DateTime.fromFormat('2024-10-07 19:00', DATE_FORMAT, {
       zone: 'America/Los_Angeles',
     }),
-    pickup: DateTime.fromFormat('2024-10-13 17:00', DATE_FORMAT, {
+    pickup: DateTime.fromFormat('2024-10-11 23:59', DATE_FORMAT, {
       zone: 'America/Los_Angeles',
     }),
     link: 'https://forms.gle/dZKHC8H3ByHjnyMH6',
