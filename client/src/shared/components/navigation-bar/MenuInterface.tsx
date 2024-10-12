a2a9b75ead49f992df082a541bd8ae484a87e674
Erick Gunawan <erick.gunawan@runchise.com>
2024-10-12 00:30:49 -0600

    removed gallery link from burger navbar

--- client/src/shared/components/navigation-bar/MenuInterface.tsx
+++ client/src/shared/components/navigation-bar/MenuInterface.tsx
@@ -1,27 +1,28 @@
-import { NavLink } from 'react-router-dom';
-import { PAGES } from '../../data/pages';
+import { NavLink } from "react-router-dom";
+import { PAGES } from "../../data/pages";
 
 export const MenuInterface = ({ isOpen }: { isOpen: boolean }) => {
   return (
     <div
       className={`absolute right-0 top-full py-5 pl-[1.12rem] pr-[1.19rem] mt-2 bg-white shadow-[-2px_2px_7px_0px_rgba(0,0,0,0.10)] -z-10 ${
-        isOpen ? 'block' : 'hidden'
+        isOpen ? "block" : "hidden"
       } animate-growDown origin-top-right`}
     >
       <div className="grid gap-y-[0.4rem]">
         {PAGES.filter(
           (page) =>
-            page.name !== 'home' &&
-            page.name !== 'merchandise' &&
-            page.name !== 'orders'
+            page.name !== "home" &&
+            page.name !== "merchandise" &&
+            page.name !== "orders" &&
+            page.name !== "gallery"
         ).map((page) => (
           <NavLink key={page.name} to={page.path}>
             {({ isActive }) => (
               <p
                 className={`${
-                  isActive ? 'text-black-permika' : 'text-light-grey'
+                  isActive ? "text-black-permika" : "text-light-grey"
                 } transition duration-500 ${
-                  isOpen ? 'block opacity-100' : 'hidden opacity-0'
+                  isOpen ? "block opacity-100" : "hidden opacity-0"
                 }`}
               >
                 {page.name}
