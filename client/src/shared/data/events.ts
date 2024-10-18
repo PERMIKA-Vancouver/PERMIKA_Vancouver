80a0930aa4699a56c60833ed4d9d72fb397157b6
Julian <julian.widjaja02@gmail.com>
2024-10-17 18:51:20 -0700

    Update misteri event

--- client/src/shared/data/events.ts
+++ client/src/shared/data/events.ts
@@ -1,34 +1,37 @@
-import { UpcomingEvent } from "../types/types";
+import { UpcomingEvent } from '../types/types';
 const { DateTime } = require('luxon');
 const DATE_FORMAT = 'YYYY-MM-DD HH:mm';
 
 // Date format 'YYYY-MM-DD HH:mm'
 const UPCOMING_EVENTS: UpcomingEvent[] = [
   {
-    name: "Waroeng 2024: Ayam Geprek",
-    date: DateTime.fromFormat('2024-10-05 19:00', 'yyyy-MM-dd HH:mm', { zone: 'America/Los_Angeles' }).toString(),
-    rsvp: "https://forms.gle/dZKHC8H3ByHjnyMH6",
-    location: "UBC, Marine Drive, Metrotown, Lougheed",
-    locationLink: "",
+    name: 'MISTERI: Misi Temukan Rahasia Identitas',
+    date: DateTime.fromFormat('2024-10-26 15:00', 'yyyy-MM-dd HH:mm', {
+      zone: 'America/Los_Angeles',
+    }).toString(),
+    rsvp: 'https://forms.gle/4iJqoDagEjvrAcCb7',
+    location: 'KJRI',
+    locationLink: 'https://maps.app.goo.gl/fDLonFuA8ZUEHx1a7',
     type: 0,
   },
 ];
 
 const UPCOMING_EVENTS_DEFAULT: UpcomingEvent = {
-  name: "No Upcoming Events",
-  date: "0000-00-00 00:00",
-  rsvp: "",
-  location: "",
-  locationLink: "",
+  name: 'No Upcoming Events',
+  date: '0000-00-00 00:00',
+  rsvp: '',
+  location: '',
+  locationLink: '',
   type: 0,
 };
 
 // const EVENT_DESCRIPTION = "Save yourself a seat for the event below!";
-const EVENT_DESCRIPTION = "Order Here!";
-const EVENT_BUTTON = "ORDER";
+const EVENT_DESCRIPTION =
+  'Unlock your mystery-solving skills, help us reveal the secret killer for the case of Mr. Julian Widjaja';
+const EVENT_BUTTON = 'RSVP Here';
 
 const HIRING_DESCRIPTION = "Be part of PERMIKA's family 2024/2025";
-const HIRING_BUTTON = "Join PERMIKA";
+const HIRING_BUTTON = 'Join PERMIKA';
 
 export {
   DATE_FORMAT,
@@ -39,4 +42,3 @@ export {
   HIRING_DESCRIPTION,
   HIRING_BUTTON,
 };
-
