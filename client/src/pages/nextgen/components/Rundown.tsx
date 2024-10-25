6b78c9be155ff1432a7103e49d52d9f6a4c8506f
Julian Widjaja <julian.widjaja02@gmail.com>
2024-10-25 01:54:31 -0700

    Nextgen: Added responsiveness to rundown

--- client/src/pages/nextgen/components/Rundown.tsx
+++ client/src/pages/nextgen/components/Rundown.tsx
@@ -3,22 +3,28 @@ import { SCHEDULE } from '../data/rundown';
 export const Rundown = () => {
   const len = SCHEDULE.length;
   return (
-    <div className="min-h-[50vh] py-36 flex items-center justify-center">
-      <div className="grid grid-cols-3 gap-28">
+    <div className="min-h-[50vh] py-36 lg:flex items-center justify-center pl-all lg:!pl-0">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-9 lg:gap-28">
         <div className="text-left col-span-1">
           <h2>Rundown</h2>
         </div>
 
-        <div className="col-span-2">
+        <div className="col-span-2 w-fit lg:w-auto">
           {SCHEDULE.map((item, index) => (
             <div
               key={index}
-              className={`flex gap-28 ${
+              className={`flex gap-6 pr-8 sm:pr-32 lg:pr-0 md:gap-28 ${
                 index !== len - 1 && 'border-b'
               } border-[#BCBCBC] ${index === 0 ? 'pb-4' : 'py-4'}`}
             >
-              <h4 className="text-black/50">{item.time}</h4>
-              <h4>{item.activity}</h4>
+              <>
+                <h4 className="hidden md:block text-black/50">{item.time}</h4>
+                <p className="block md:hidden text-black/50">{item.time}</p>
+              </>
+              <>
+                <h4 className="hidden md:block">{item.activity}</h4>
+                <p className="block md:hidden">{item.activity}</p>
+              </>
             </div>
           ))}
         </div>
