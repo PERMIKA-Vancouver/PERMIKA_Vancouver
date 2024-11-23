97c2e19f2eee6d18f688d284cf960cb22619c9ad
Erick Gunawan <erick.gunawan@runchise.com>
2024-11-23 03:13:28 -0700

    changed href to a google.com placeholder (change it to the pdf)

--- client/src/pages/nextgen/components/Hero.tsx
+++ client/src/pages/nextgen/components/Hero.tsx
@@ -18,7 +18,10 @@ export const Hero = () => {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our quick tutorial.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
+              <a
+                href="www.google.com"
+                className="font-semibold text-indigo-600"
+              >
                 <span className="absolute inset-0" aria-hidden="true"></span>
                 Read more <span aria-hidden="true">&rarr;</span>
               </a>
@@ -35,7 +38,7 @@ export const Hero = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="www.google.com"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Learn more <span aria-hidden="true">→</span>
