4d96e6a0765de4c20bbca160ffc0ddd16edad38f
Erick Gunawan <erick.gunawan@runchise.com>
2025-01-24 20:32:27 -0700

    new waroeng

--- client/src/pages/home/components/Waroeng.tsx
+++ client/src/pages/home/components/Waroeng.tsx
@@ -1,10 +1,10 @@
-import { CustomButton } from '../../../shared/components/CustomButton';
+import { CustomButton } from "../../../shared/components/CustomButton";
 import {
   getNextWaroeng,
   getNextWaroengLink,
   isNextWaroeng,
   isWaroengPreOrderPassed,
-} from '../../../shared/utils/UpcomingWaroengUtils';
+} from "../../../shared/utils/UpcomingWaroengUtils";
 
 export const Waroeng = () => {
   const nextWaroeng = getNextWaroeng();
@@ -12,23 +12,22 @@ export const Waroeng = () => {
   if (!isNextWaroeng(nextWaroeng)) {
     return <div></div>;
   }
-
   return (
     <div className="ml-all mt-24 mr-[10%] sm:mr-28">
       <div className="flex justify-between flex-wrap">
         <h2 className="mb-4">Waroeng '24 Presents</h2>
         {!isWaroengPreOrderPassed(nextWaroeng) && (
           <CustomButton
-            text={'Pre-order'}
-            className={'self-center'}
+            text={"Pre-order"}
+            className={"self-center"}
             link={getNextWaroengLink(nextWaroeng)}
           />
         )}
       </div>
       <div className="flex mt-8 w-full">
-        <img src={''} alt="Waroeng" className="w-1/3" />
-        <img src={''} alt="Waroeng" className="w-1/3" />
-        <img src={''} alt="Waroeng" className="w-1/3" />
+        <img src={""} alt="Waroeng" className="w-1/3" />
+        <img src={""} alt="Waroeng" className="w-1/3" />
+        <img src={""} alt="Waroeng" className="w-1/3" />
       </div>
     </div>
   );
--- client/src/shared/data/waroeng.ts
+++ client/src/shared/data/waroeng.ts
@@ -1,18 +1,18 @@
-import { UpcomingWaroeng } from '../types/types';
-const { DateTime } = require('luxon');
+import { UpcomingWaroeng } from "../types/types";
+const { DateTime } = require("luxon");
 
-const DATE_FORMAT = 'yyyy-MM-dd HH:mm';
+const DATE_FORMAT = "yyyy-MM-dd HH:mm";
 
 // Date format 'YYYY-MM-DD HH:mm'
 const UPCOMING_WAROENG: UpcomingWaroeng[] = [
   {
-    date: DateTime.fromFormat('2024-10-11 23:59', DATE_FORMAT, {
-      zone: 'America/Los_Angeles',
+    date: DateTime.fromFormat("2025-01-25 23:59", DATE_FORMAT, {
+      zone: "America/Los_Angeles",
     }),
-    pickup: DateTime.fromFormat('2024-10-13 17:00', DATE_FORMAT, {
-      zone: 'America/Los_Angeles',
+    pickup: DateTime.fromFormat("2025-01-30 17:00", DATE_FORMAT, {
+      zone: "America/Los_Angeles",
     }),
-    link: 'https://forms.gle/dZKHC8H3ByHjnyMH6',
+    link: "https://forms.gle/dZKHC8H3ByHjnyMH6",
   },
 ];
 
