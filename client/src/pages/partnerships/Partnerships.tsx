f30481e4fbf6ebe5061ceed9ddeadd908ff03811
Reynella Liebig <reynella@tapta.lk>
2025-02-03 16:36:25 -0800

    resolve warnings

--- client/src/pages/partnerships/Partnerships.tsx
+++ client/src/pages/partnerships/Partnerships.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
 import { LandingPage } from './components/LandingPage';
 import { Sponsors } from './components/Sponsors';
 import { Opportunities } from './components/Opportunities';
-import { Footer } from '../../shared/components/Footer';
-import { MOBILE_WIDTH, TABLET_POTRAIT_WIDTH } from "../../shared/data/common";
 
 export const Partnerships = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   return (
     <div>
         <LandingPage />
