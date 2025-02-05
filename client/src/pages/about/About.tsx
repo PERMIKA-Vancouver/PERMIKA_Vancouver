f473b1f51d1a5969d5a464cf85c9acd6db95f0c1
Reynella Liebig <reynella@tapta.lk>
2025-02-04 20:15:34 -0800

    deleted about page updates

--- client/src/pages/about/About.tsx
+++ client/src/pages/about/About.tsx
@@ -1,21 +1,4 @@
 import { UpcomingPage } from '../../shared/layouts/UpcomingPage';
-import { LandingPage } from './components/LandingPage';
-import { Preview } from './components/Preview';
-import { Teams } from './components/Teams';
-
-// export const About = () => {
-//   return (
-//     <div>
-//         <LandingPage />
-//         <div className="relative w-full ">
-//           <div className='relative pt-[100vh] w-full'>
-//             <Preview />
-//             <Teams />
-//           </div>
-//         </div>
-//     </div>
-//   );
-// };
 
 export const About = () => {
   return (
